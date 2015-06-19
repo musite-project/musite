7e67c3c2ac934cbfc2ff5459d2a73a232342ae61
Jacques Peron <cataclop@hotmail.com>
2015-06-19 21:55:20 +0200

    Pdf: modification de l'aperçu
    L'intégration en base 64 de gros pdfs faisait planter le navigateur ; on
    remplace ici par une balise object "classique".

--- deps/ext/pdf.py
+++ deps/ext/pdf.py
@@ -2,9 +2,12 @@
 """Gestion des fichiers pdf
 """
 import os
+import shutil
 from deps import HTMLTags as h
+from bottle import static_file
 from base64 import b64encode
 from etc import config as cfg
+from deps.outils import url
 
 
 class Document:
@@ -27,12 +30,13 @@ class Document:
     def afficher(self):
         """Contenu du pdf embarqué dans un code html
         """
+        self.preparer()
         return h.OBJECT(
-            data="data:application/pdf;base64,{}".format(self.contenu),
+            data="{}".format(url(self._fichier())),
             Type="application/pdf",
             width="100%",
             height="100%"
-            )
+        )
 
     @property
     def contenu(self):
@@ -40,6 +44,27 @@ class Document:
         """
         with open(self.fichier, "rb") as doc:
             return b64encode(doc.read()).decode('ascii')
+
+    def _fichier(self, ext='pdf'):
+        """Chemin absolu
+        """
+        return os.path.join(
+            cfg.PWD, cfg.STATIC, 'docs', ext,
+            self._fichierrelatif(ext)
+        )
+
+    def _fichierrelatif(self, ext=None):
+        """Chemin vers un fichier portant ce nom avec une autre extension
+        """
+        return os.path.splitext(self.chemin)[0] \
+            + ('.' + ext if ext else '.' + self.ext if self.ext else '')\
+            .replace('/', os.path.sep)
+
+    def preparer(self):
+        if not os.path.isfile(self._fichier()) \
+        or os.path.getmtime(self._fichier()) < os.path.getmtime(self.fichier):
+            os.makedirs(os.path.dirname(self._fichier()))
+            shutil.copy(self.fichier, self._fichier())
 
     def supprimer(self):
         """Suppression du document
